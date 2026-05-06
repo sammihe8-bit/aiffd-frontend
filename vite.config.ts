201268bdcbebb6c5df93f35d33011ee549c883a7
sammihe8-bit <sammihe8@gmail.com>
2026-05-05 23:16:38 -0700

    feat: add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://eloquent-enthusiasm-production.up.railway.app',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  }
+})
